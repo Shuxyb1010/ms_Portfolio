1a87f94e758de3506cee3a523a07c2dea4ba5e16
Muhammad Shuayb <mshuayb02@gmail.com>
2023-09-30 23:00:26 +0100

    Updates to Projects and Skills

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,45 +1,42 @@
-import Project1 from "../../assets/project1.webp";
 import Project2 from "../../assets/project2.webp";
 import Project3 from "../../assets/project3.webp";
-
+import LMS from "../../assets/LMS.jpg";
 
 export const projectsData = [
-    {
-        id: 1,
-        image: Project1,
-        title: "Project 1",
-        category: "A",
-        tags: "React, CSS, Python"
-        
-    },
-    {
-        id: 2,
-        image: Project2,
-        title: "Project 2",
-        category: "B",
-        tags: "React, mongoDB, Tailwind"
-    },
-    {
-        id: 3,
-        image: Project3,
-        title: "Project 3",
-        category: "C",
-        tags: "Next.js, Tailwind, Firebase"
-    },
+  {
+    id: 1,
+    image: LMS,
+    title: "Learning Management",
+    category: "A",
+    tags: "Next, React, Tailwind, Prisma, MYSQL, Stripe, Mux",
+  },
+  {
+    id: 2,
+    image: Project2,
+    title: "Project 2",
+    category: "B",
+    tags: "React, mongoDB, Tailwind",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Project 3",
+    category: "C",
+    tags: "Next.js, Tailwind, Firebase",
+  },
 ];
 
-
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "A",
-    },
-    {
-        name: "B",
-    },
-    {
-        name: "C",
-    },
+  {
+    name: "All",
+  },
+  {
+    name: "A",
+  },
+  {
+    name: "B",
+  },
+  {
+    name: "C",
+  },
 ];
